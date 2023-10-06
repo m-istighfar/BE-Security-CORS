ff0c9b5654a426f5ebbdf6ae386e4cbf46066bde
Tifar <istighfar.amal@gmail.com>
2023-10-06 23:41:51 +0700

    new origin

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,14 +1,17 @@
 const cors = require("cors");
 
 module.exports = (app) => {
-  const whitelist = ["http://localhost:5000", "http://localhost:5001"];
+  const whitelist = [
+    "https://w-15-clientx.netlify.app/",
+    "https://w-15-clienty.netlify.app/",
+  ];
   const corsOptionsDelegate = (req, callback) => {
     let corsOptions;
     if (whitelist.indexOf(req.header("Origin")) !== -1) {
       corsOptions = {
         origin: true,
         methods:
-          req.header("Origin") === "http://localhost:5000"
+          req.header("Origin") === "https://w-15-clientx.netlify.app/"
             ? ["GET", "POST"]
             : ["GET", "POST", "PUT", "DELETE"],
       };
